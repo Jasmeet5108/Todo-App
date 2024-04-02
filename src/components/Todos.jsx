ebab30288abfc3f7450635ff13b57729e6b1c1f3
Jasmeet Singh <jasmeet5108k@gmail.com>
2024-04-02 17:13:31 +0530

    Message

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -8,7 +8,10 @@ const Manager = () => {
         name: "",
         todo: "",
     });
+
     const [todoArray, setTodoArray] = useState([]);
+
+    const [editing, setEditing] = useState(false);
 
     // Effect to load todos from local storage when component mounts
     useEffect(() => {
@@ -19,20 +22,21 @@ const Manager = () => {
     }, []);
 
     // Function to save password to local storage and state
-    const saveData = () => {
+    const saveData = (e) => {
+        e.preventDefault()
         setTodoArray([...todoArray, { ...form, id: uuidv4() }])
         localStorage.setItem("todos", JSON.stringify([...todoArray, { ...form, id: uuidv4() }]))
         console.log([...todoArray, { ...form, id: uuidv4() }]);
         setForm({ name: "", todo: "" })
+        setEditing(false)
     };
 
     const editData = (id) => {
-        console.log("Editing data with ID", id);
+        setEditing(true)
         setForm(todoArray.filter(item => item.id === id)[0])
         setTodoArray(todoArray.filter(item => item.id !== id))
     }
     const deleteData = (id) => {
-        console.log("Deleting data with ID", id);
         setTodoArray(todoArray.filter(item => item.id !== id)) // Only return those objects from the array which do not have the same ID as mentioned
         localStorage.setItem("todos", JSON.stringify(todoArray.filter(item => item.id !== id)))
     }
@@ -50,58 +54,77 @@ const Manager = () => {
             <div className="my-container">
 
                 {/* Header */}
-                <p className='text-white text-2xl text-center mt-10 font-semibold underline underline-offset-4'>Create your todos here</p>
+                <p className='text-white text-2xl text-center mt-10 font-semibold underline underline-offset-4'>Create Your Todos Here</p>
 
                 {/* Form */}
-                <div className='text-black flex flex-col p-4 gap-8 items-center mt-5 w-[350px] sm:w-[600px] mx-auto'>
-                    <input name="name" onChange={handleChange} value={form.name} className='rounded-xl border border-green-500 w-full px-4 py-1' type="text" id='' placeholder='Enter your name' />
-                    <input name="todo" onChange={handleChange} value={form.todo} className='rounded-xl border border-green-500 w-full px-4 py-1' type="text" id='' placeholder='Enter task' />
-                    <button onClick={saveData} className='w-fit font-semibold text-white flex justify-center items-center gap-2 bg-cyan-600 rounded-full px-8 py-2 border border-green-900'>
-                        Add Todo <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" color="#ffffff" fill="none">
-                            <path d="M12 4V20M20 12H4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
-                    </button>
-                </div>
+                <form onSubmit={saveData}>
+                    <div className='text-black flex flex-col p-2 gap-8 items-center mt-5 w-[340px] sm:w-[600px] mx-auto'>
+
+                        <input name="name" onChange={handleChange} value={form.name} className='rounded-xl border border-green-500 w-full px-4 py-1' type="text" id='' placeholder='Enter your name' required />
+
+                        <input name="todo" onChange={handleChange} value={form.todo} className='rounded-xl border border-green-500 w-full px-4 py-1' type="text" id='' placeholder='Enter task here' required />
+
+                        <button type='submit' className='w-fit font-semibold text-white flex justify-center items-center gap-2 bg-cyan-600 rounded-full px-8 py-2 border border-green-900'>
+                            Add Todo <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" color="#ffffff" fill="none">
+                                <path d="M12 4V20M20 12H4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </button>
+                    </div>
+                </form>
 
                 {/* Display saved todos */}
-                <div className="todos mt-5">
+                <div className="todos mt-7">
                     <h2 className='font-semibold text-2xl py-4 text-white text-center underline underline-offset-4'>Your Todo List</h2>
-                    {todoArray.length === 0 && <div className='w-52 py-1 border border-purple-600 rounded-md mx-auto text-white text-center font-semibold bg-gradient-to-r from-violet-800 to-purple-600'>Well, Nothing's here yet!!</div>}
-                    {todoArray.length != 0 && <table className="table-auto w-[360px] mx-auto rounded-md overflow-hidden sm:w-[600px] lg:w-[1000px]">
-                        <thead className='bg-sky-800 text-white'>
-                            <tr>
-                                <th className='py-2'>Name</th>
-                                <th className='py-2'>Tasks</th>
-                                <th className='py-2'>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody className='bg-sky-100'>
-                            {todoArray.map((item, index) => {
-                                return <tr key={index}>
-                                    <td className='py-2 border border-black text-center'>
-                                        <div className='flex items-center justify-center'>
-                                            <span>{item.name}</span>
-                                        </div>
-                                    </td>
-                                    <td className='py-2 border border-black text-center'>
-                                        <div className='flex items-center justify-center'>
-                                            <span>{item.todo}</span>
-                                        </div>
-                                    </td>
-                                    <td className='py-2 border border-black text-center space-x-3 flex justify-center'>
-                                        {/* Edit */}
-                                        <button onClick={() => editData(item.id)} className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-1 px-2 rounded">
-                                            Edit
-                                        </button>
-                                        {/* Delete */}
-                                        <button onClick={() => deleteData(item.id)} className="bg-rose-500 hover:bg-rose-700 text-white font-bold py-1 px-2 rounded">
-                                            Delete
-                                        </button>
-                                    </td>
+                    {
+                        todoArray.length === 0 &&
+                        <>
+                            <div className='w-40 mt-3 py-1 border border-white rounded-md mx-auto text-white text-center font-semibold bg-gradient-to-l from-fuchsia-500 to-pink-600'>{editing ? "Edit in the form" : "No Todos Found"}</div>
+                            {editing === false && <div>
+                                <img src="/nothing-here-unscreen.gif" className='block mx-auto mt-1 pl-4 sm:w-44' width={120} alt="Nothing here" />
+                            </div>}
+                        </>
+                    }
+
+                    {
+                        todoArray.length != 0 &&
+                        <table className="table-auto w-[350px] mx-auto rounded-md overflow-hidden sm:w-[600px] lg:w-[1000px]">
+                            <thead className='bg-sky-800 text-white'>
+                                <tr>
+                                    <th className='py-2 border border-black text-center'>Name</th>
+                                    <th className='py-2 border border-black text-center'>Tasks</th>
+                                    <th className='py-2 border border-black text-center'>Actions</th>
                                 </tr>
-                            })}
-                        </tbody>
-                    </table>}
+                            </thead>
+                            <tbody className='bg-sky-100'>
+                                {
+                                    todoArray.map((item, index) => {
+                                        return <tr key={index}>
+                                            <td className='py-2 border border-black text-center'>
+                                                <div className='flex items-center justify-center'>
+                                                    <span>{item.name}</span>
+                                                </div>
+                                            </td>
+                                            <td className='py-2 border border-black text-center'>
+                                                <div className='flex items-center justify-center'>
+                                                    <span>{item.todo}</span>
+                                                </div>
+                                            </td>
+                                            <td className='py-2 border border-black text-center space-x-3 flex justify-center'>
+                                                {/* Edit */}
+                                                <button onClick={() => editData(item.id)} className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-1 px-2 rounded">
+                                                    Edit
+                                                </button>
+                                                {/* Delete */}
+                                                <button onClick={() => deleteData(item.id)} className="bg-rose-500 hover:bg-rose-700 text-white font-bold py-1 px-2 rounded">
+                                                    Delete
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    }
                 </div>
             </div>
         </>
