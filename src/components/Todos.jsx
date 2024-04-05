53943d4bb9cedcc04a0a3201595168b5b82682ac
Jasmeet Singh <jasmeet5108k@gmail.com>
2024-04-05 15:53:23 +0530

    Updations

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -54,17 +54,17 @@ const Manager = () => {
             <div className="my-container">
 
                 {/* Header */}
-                <p className='text-white text-2xl text-center mt-10 font-semibold underline underline-offset-4'>Create Your Todos Here</p>
+                <p className='text-white text-2xl md:text-3xl text-center mt-10 font-semibold underline underline-offset-4'>Create Your Todos Here</p>
 
                 {/* Form */}
-                <form onSubmit={saveData}>
-                    <div className='text-black flex flex-col p-2 gap-8 items-center mt-5 w-[340px] sm:w-[600px] mx-auto'>
+                <form onSubmit={saveData} className='md:mt-7'>
+                    <div className='text-black flex flex-col p-2 gap-8 items-center mt-5 w-[340px] sm:w-[600px] md:w-[700px] lg:w-[900px] mx-auto'>
 
-                        <input name="name" onChange={handleChange} value={form.name} className='rounded-xl border border-green-500 w-full px-4 py-1' type="text" id='' placeholder='Enter your name' required />
+                        <input name="name" onChange={handleChange} value={form.name} className='rounded-xl md:text-xl border border-indigo-500 w-full px-4 py-1 lg:h-[40px]' type="text" id='' placeholder='Enter your name' required />
 
-                        <input name="todo" onChange={handleChange} value={form.todo} className='rounded-xl border border-green-500 w-full px-4 py-1' type="text" id='' placeholder='Enter task here' required />
+                        <input name="todo" onChange={handleChange} value={form.todo} className='rounded-xl md:text-xl border border-indigo-500 w-full px-4 py-1 lg:h-[40px]' type="text" id='' placeholder='Enter task here' required />
 
-                        <button type='submit' className='w-fit font-semibold text-white flex justify-center items-center gap-2 bg-cyan-600 rounded-full px-8 py-2 border border-green-900'>
+                        <button type='submit' className='w-fit font-semibold text-white flex justify-center md:text-xl items-center gap-2 bg-cyan-600 hover:bg-cyan-500 rounded-full px-8 py-2 border border-green-900'>
                             Add Todo <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" color="#ffffff" fill="none">
                                 <path d="M12 4V20M20 12H4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
@@ -73,21 +73,21 @@ const Manager = () => {
                 </form>
 
                 {/* Display saved todos */}
-                <div className="todos mt-7">
-                    <h2 className='font-semibold text-2xl py-4 text-white text-center underline underline-offset-4'>Your Todo List</h2>
+                <div className="todos mt-7 md:mt-10">
+                    <h2 className='font-semibold text-2xl md:text-3xl py-4 text-white text-center underline underline-offset-4'>Your Todo List</h2>
                     {
                         todoArray.length === 0 &&
                         <>
-                            <div className='w-40 mt-3 py-1 border border-white rounded-md mx-auto text-white text-center font-semibold bg-gradient-to-l from-fuchsia-500 to-pink-600'>{editing ? "Edit in the form" : "No Todos Found"}</div>
                             {editing === false && <div>
-                                <img src="/nothing-here-unscreen.gif" className='block mx-auto mt-1 pl-4 sm:w-44' width={120} alt="Nothing here" />
+                                <img src="/nothing-here-unscreen.gif" className='block mx-auto pl-4 sm:w-44' width={120} alt="Nothing here" />
                             </div>}
+                            <div className='w-40 md:text-lg py-1 border border-white rounded-md mx-auto text-white text-center font-semibold'>{editing ? "Edit in the form" : "The list is empty!"}</div>
                         </>
                     }
 
                     {
                         todoArray.length != 0 &&
-                        <table className="table-auto w-[350px] mx-auto rounded-md overflow-hidden sm:w-[600px] lg:w-[1000px]">
+                        <table className="table-auto sm:mt-4 w-[350px] mx-auto rounded-md overflow-hidden sm:w-[600px] lg:w-[1000px]">
                             <thead className='bg-sky-800 text-white'>
                                 <tr>
                                     <th className='py-2 border border-black text-center'>Name</th>
@@ -101,21 +101,21 @@ const Manager = () => {
                                         return <tr key={index}>
                                             <td className='py-2 border border-black text-center'>
                                                 <div className='flex items-center justify-center'>
-                                                    <span>{item.name}</span>
+                                                    <span className='md:text-xl'>{item.name}</span>
                                                 </div>
                                             </td>
                                             <td className='py-2 border border-black text-center'>
                                                 <div className='flex items-center justify-center'>
-                                                    <span>{item.todo}</span>
+                                                    <span className='md:text-xl'>{item.todo}</span>
                                                 </div>
                                             </td>
-                                            <td className='py-2 border border-black text-center space-x-3 flex justify-center'>
+                                            <td className='py-2 text-center border border-black space-x-3 flex justify-center'>
                                                 {/* Edit */}
-                                                <button onClick={() => editData(item.id)} className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-1 px-2 rounded">
+                                                <button onClick={() => editData(item.id)} className="bg-emerald-500 hover:bg-emerald-700 text-white md:text-xl font-semibold py-1 px-2 md:px-3 rounded">
                                                     Edit
                                                 </button>
                                                 {/* Delete */}
-                                                <button onClick={() => deleteData(item.id)} className="bg-rose-500 hover:bg-rose-700 text-white font-bold py-1 px-2 rounded">
+                                                <button onClick={() => deleteData(item.id)} className="bg-rose-500 hover:bg-rose-700 text-white md:text-xl font-semibold py-1 px-2 md:px-3 rounded">
                                                     Delete
                                                 </button>
                                             </td>
